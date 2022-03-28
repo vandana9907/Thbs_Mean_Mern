018d43d29b2987efac4b0df2062201236fe31f78
Vandana Gurram <vandana_gurram@thbs.india.com>
2022-03-28 11:21:52 +0530

    oject

---
+++ javascript/array.js
@@ -0,0 +1,21 @@
+let contactList = []; //declear of empty array 
+console.log(typeof contactList) // object
+console.log(contactList);
+
+let contactL = new Array(); // declear array with new keyword
+console.log(contactL);
+
+contactL = ['ten', 20, true, 'hello']; // initlization or assing value to array
+console.log(contactL);
+
+//decleartion and initlization at same time
+let classList = ['99th', '93', 'thbs'];
+console.log(classList);
+//accssing value from array;
+console.log(classList[2]); // classList[2]
+console.log(classList["1"]); // index as string qoeutes
+//update values inside arraya 
+//fist access array postion and = to assing new value.
+
+classList[2] = "THIS";
+console.log(classList);
