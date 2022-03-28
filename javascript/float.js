4d8ab4c3ef37421005394123a5f5de5d3872aee6
Vandana Gurram <vandana_gurram@thbs.india.com>
2022-03-28 17:24:43 +0530

    lets

---
+++ javascript/float.js
@@ -0,0 +1,8 @@
+function addNumber(n1, n2) {
+    return n1 + n2;
+}
+
+console.log(addNumber(10, 20));
+//------------------------------this is anthor process(new)
+let myadd = (n1, n2) => n1 + n2;
+console.log(myadd(10, 20));// 15
